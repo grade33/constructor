0516693a042d1449b9493c32cfef0fc5a399ae08
grade33 <baledg3@gmail.com>
2023-05-22 19:23:05 +0400

    Deploy gallery-8-1

---
+++ src/scripts/components/gallery/gallery-6-1.js
@@ -0,0 +1,12 @@
+new window.helpFunctions.swiper.Swiper('.swiper.gallery-6-1__slider', {
+  modules: [window.helpFunctions.swiper.Autoplay],
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 1000,
+  spaceBetween: 10,
+  keyboard: true,
+  rewind: true,
+  simulateTouch: false,
+  slidesPerView: 1.113,
+});
