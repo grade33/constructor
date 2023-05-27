9e9c1ed2870b672501cab51dbf3e7ae41cd32504
grade33 <baledg3@gmail.com>
2023-05-27 14:24:58 +0400

    Deploy gallery-7-1 section

---
+++ src/scripts/components/gallery/gallery-7-1.js
@@ -0,0 +1,22 @@
+new window.helpFunctions.swiper.Swiper('.swiper.gallery-7-1__slider', {
+  modules: [window.helpFunctions.swiper.Navigation, window.helpFunctions.swiper.Autoplay],
+  navigation: {
+    prevEl: '.gallery-7-1__slider-btn_prev',
+    nextEl: '.gallery-7-1__slider-btn_next',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+  speed: 1000,
+  slidesPerView: 1.112,
+  spaceBetween: 12,
+  keyboard: true,
+  loop: true,
+  centeredSlides: true,
+  breakpoints: {
+    [window.helpFunctions.breakpointsMin.md]: {
+      spaceBetween: 30,
+      slidesPerView: 1.3,
+    },
+  },
+});
