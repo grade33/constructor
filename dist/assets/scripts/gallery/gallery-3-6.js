317422bc5cc38f48b54df0dcde7c80647250784b
grade33 <baledg3@gmail.com>
2023-05-30 20:52:03 +0400

    Deploy change gallery mobile slider

--- dist/assets/scripts/gallery/gallery-3-6.js
+++ dist/assets/scripts/gallery/gallery-3-6.js
@@ -1,16 +1,20 @@
 /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
 new window.helpFunctions.swiper.Swiper('.swiper.gallery-3-6__slider', {
-  modules: [window.helpFunctions.swiper.Autoplay],
+  modules: [window.helpFunctions.swiper.Navigation, window.helpFunctions.swiper.Autoplay],
+  navigation: {
+    prevEl: '.gallery-3-6__slider-btn_prev',
+    nextEl: '.gallery-3-6__slider-btn_next'
+  },
   autoplay: {
     delay: 5000
   },
   speed: 1000,
-  spaceBetween: 10,
+  slidesPerView: 1.112,
+  spaceBetween: 12,
   keyboard: true,
-  rewind: true,
-  simulateTouch: false,
-  slidesPerView: 1.113
+  loop: true,
+  centeredSlides: true
 });
 /******/ })()
 ;
--- dist/assets/scripts/gallery/gallery-6-1.js
+++ dist/assets/scripts/gallery/gallery-6-1.js
@@ -1,16 +1,20 @@
 /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
 new window.helpFunctions.swiper.Swiper('.swiper.gallery-6-1__slider', {
-  modules: [window.helpFunctions.swiper.Autoplay],
+  modules: [window.helpFunctions.swiper.Navigation, window.helpFunctions.swiper.Autoplay],
+  navigation: {
+    prevEl: '.gallery-6-1__slider-btn_prev',
+    nextEl: '.gallery-6-1__slider-btn_next'
+  },
   autoplay: {
     delay: 5000
   },
   speed: 1000,
-  spaceBetween: 10,
+  slidesPerView: 1.112,
+  spaceBetween: 12,
   keyboard: true,
-  rewind: true,
-  simulateTouch: false,
-  slidesPerView: 1.113
+  loop: true,
+  centeredSlides: true
 });
 /******/ })()
 ;
