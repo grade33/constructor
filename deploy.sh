c8350935d2e3ee5cc7b6c71d2554481bf12263af
grade33 <baledg3@gmail.com>
2023-05-30 22:06:45 +0400

    Deploy hide sliders in gallery to mobile version01~

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ git add .
 git commit -m "Deploy $1"
 git push origin main
 git push origin2 main
-git subtree push --prefix dist origin gh-pages
-git subtree push --prefix dist origin2 gh-pages
+npm run deploy
+npm run deploy2
